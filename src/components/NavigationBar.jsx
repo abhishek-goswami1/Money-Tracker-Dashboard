d573ecaffce6863fceaa96d8fc5b67242dd8ce01
abhi001-q <abhishek987ff@gmail.com>
2026-07-19 12:28:34 +0200

    feat: enhance NavigationBar with dynamic visibility for navlink names and add Dashboard component

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -5,7 +5,7 @@ import { IoMdAnalytics } from "react-icons/io";
 import { TbTransactionRupee } from "react-icons/tb";
 import { MdHelpCenter } from "react-icons/md";
 import { MdArrowForward } from "react-icons/md";
-import { motion } from "motion/react"
+import { motion } from "motion/react";
 const navlinks = [
   { name: "Dashboard", icon: <MdDashboard /> },
   { name: "Activity", icon: <RxActivityLog /> },
@@ -13,11 +13,11 @@ const navlinks = [
   { name: "Transactions", icon: <TbTransactionRupee /> },
   { name: "Help Center", icon: <MdHelpCenter /> },
 ];
- 
+
 const varinats = {
-  expanded: {width: "20%"},
-  nonExpanded: {width: "5%"}
-}
+  expanded: { width: "20%" },
+  nonExpanded: { width: "5" },
+};
 
 function NavigationBar() {
   const [activeNavIndex, setActiveIndex] = useState(0);
@@ -26,21 +26,25 @@ function NavigationBar() {
 
   return (
     <motion.div
-     animate={isExpanded ? "expanded" : "nonExpanded"}
-     variants={varinats}
-     className="w-1/6 h-screen relative flex flex-col border border-r px-10 py-12">
+      animate={isExpanded ? "expanded" : "nonExpanded"}
+      variants={varinats}
+      className={`w-1/5 h-screen relative flex flex-col border border-r py-12 ${
+        isExpanded ? "px-10" : "px-4"
+      }`}
+    >
       <div className="flex space-x-2 items-center">
         <img
           className="w-10 h-10 rounded-full"
           src="https://img.magnific.com/free-psd/money-illustration-isolated_23-2151568514.jpg"
           alt="Logo"
         />
-        <span>Money Tracker</span>
+        <span className={isExpanded ? "block" : "hidden"}>Money Tracker</span>
       </div>
 
-      <div 
-      onClick={() => setIsExpanded(!isExpanded)}
-      className=" w-6 h-6 bg-[#ff8c8c] rounded-full absolute -right-3 top-12 flex itmes-center justify-center text-white ">
+      <div
+        onClick={() => setIsExpanded(!isExpanded)}
+        className=" w-6 h-6 bg-[#ff8c8c] rounded-full absolute -right-3 top-12 flex itmes-center justify-center text-white "
+      >
         <MdArrowForward className=" mt-1 w-4 " />
       </div>
 
@@ -64,7 +68,9 @@ function NavigationBar() {
               onClick={() => setActiveIndex(index)}
             >
               <span className="text-xl flex items-center">{item.icon}</span>
-              <span>{item.name}</span>
+              <span className={isExpanded ? "block" : "hidden"}>
+                {item.name}
+              </span>
             </div>
           );
         })}
